3a80e374f3a01d1693fe61a4d1457b73dc892314
GonzalezSainzR <Raygs2000@gmail.com>
2015-01-30 09:15:17 -0800

    1/30/15

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "Web Sever Is Running Locally";
+        require_once("controller/create-db.php");
         ?>
     </body>
 </html>
