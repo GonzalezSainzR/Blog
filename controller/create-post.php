3f01fc9dd5bda816cfe5bd69628b6a8ba2c873a7
GonzalezSainzR <Raygs2000@gmail.com>
2015-02-12 15:01:14 -0800

    2/12/15

--- controller/create-post.php
+++ controller/create-post.php
@@ -4,11 +4,11 @@ require_once (__DIR__ . "/../model/config.php");
 $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 $post = filter_input (INPUT_POST, "post", FILTER_SANITIZE_STRING);
 
-$query = $connection->query("INSERT INTO posts, SET title = '$title', post = '$post'");
+$query = $_SESSION["connection"]->query("INSERT INTO posts, SET title = '$title', post = '$post'");
 
 if($query) {
     echo "<p>Successfuly inserted post: $title</p>";
 }
 else {
-    echo "<p>$connection->error</p>";
+    echo "<p>" . $_SESSION["connection"]->error . "</p>";
 }
